0e7eae48190d1ac39ac2d905222822d1effab2fa
frederic.razafimampiandra <f.razafimampiandra@insyniumgroup.com>
2021-03-11 11:17:01 +0100

    changer la méthode d'appel pour la récuperation produit dans dolibarr

---
+++ htdocs/cyberoffice/testappel.php
@@ -0,0 +1,399 @@
+<?php
+
+// set post fields
+$post = [
+    'username' => 'user1',
+    'password' => 'passuser1',
+    'gender'   => 1,
+];
+
+$params = Array
+(
+    Array
+        (
+            "id_product" => "44-0000",
+            "ean13" => "5601788500000",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "",
+            "name" => "COMBI SHORT LAGON",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850000,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" => "https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester"
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        ),
+
+    Array
+        (
+            "id_product" => "44-282",
+            "ean13" => "5601788500017",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "Taille : 34, Couleur : Noir",
+
+            "name" => "COMBI SHORT LAGON 34 Noir",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850001,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" => "https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester"
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        ),
+
+    Array
+        (
+            "id_product" => "44-283",
+            "ean13" => "5601788500024",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "Taille : 36, Couleur : Noir",
+
+            "name" => "COMBI SHORT LAGON 36 Noir",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850002,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" =>"https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester"
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        ),
+
+    Array
+        (
+            "id_product" => "44-284",
+            "ean13" => "5601788500031",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "Taille : 38, Couleur : Noir",
+
+            "name" => "COMBI SHORT LAGON 38 Noir",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850003,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" => "https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester"
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        ),
+
+    Array
+        (
+            "id_product" => "44-285",
+            "ean13" => "5601788500048",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "Taille : 40, Couleur : Noir",
+
+            "name" => "COMBI SHORT LAGON 40 Noir",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850004,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" => "https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester"
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        ),
+
+    Array
+        (
+            "id_product" => "44-286",
+            "ean13" => "5601788500055",
+            "upc" => "",
+            "isbn" => "",
+            "price" => 20.184332,
+            "height" => 0.000000,
+            "width" => 0.000000,
+            "depth" => 0.000000,
+            "weight" => 0.218000,
+            "description" => "",
+            "description_short" => "Taille : 42, Couleur : Noir",
+
+            "name" => "COMBI SHORT LAGON 42 Noir",
+            "real_name" => "COMBI SHORT LAGON",
+            "tax_rate" => 8.5,
+            "reference" => 560178850005,
+            "active" => 0,
+            "quantity" => 0,
+            "warehouse" => 1,
+            "image" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+            "images" => Array
+                (
+                    Array
+                        (
+                            "url" => "https://preprod.kenza.re/140/combishort-lago-femme.jpg",
+                            "name" => "cover44-140"
+                        )
+
+                ),
+
+            "category" => "119-13-20-14-2-10",
+            "product_url" => "https://preprod.kenza.re/combi-short/44-combishort-lago-femme-5601788500000.html",
+            "manufacturer" => "Kenza Mode Métisse",
+            "id_manufacturer" => 3,
+            "eco_tax" => 0.000000,
+            "match" => "{ref}",
+            "wholesale_price" => 0.000000,
+            "unit_price_impact" => "",
+            "features" => Array
+                (
+                    Array
+                        (
+                            "id_feature" => 1,
+                            "id_feature_value" => 1,
+                            "feature_value_lang" => "Polyester",
+                        )
+
+                ),
+
+            "WEIGHT_UNIT" => "kg",
+            "VOLUME_UNIT" => "cl",
+            "DIMENSION_UNIT" => "cm",
+            "LOCALELANG" => "fr",
+            "specificprice" => Array
+                (
+                )
+
+        )
+
+);
+
+$fields_string = http_build_query($params);
+/*echo '<pre>';
+print_r($fields_string);
+die();*/
+$ch = curl_init();
+curl_setopt($ch,CURLOPT_URL, 'http://localhost/kenza/htdocs/cyberoffice/server_product.rest.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($params));
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+curl_setopt($ch, CURLOPT_POST, 1);
+
+// execute!
+$response = curl_exec($ch);
+
+// close the connection, release resources used
+curl_close($ch);
+
+header("Content-Type:application/json");
+// do anything you want with your response
+
+print $response;
