a23c11e90e79218b44d53e253d3b8c16e35acf17
frederic.razafimampiandra <f.razafimampiandra@insyniumgroup.com>
2021-04-15 07:32:32 +0200

    modification sql

--- sql_add_code_couleur_dans_llx_product_attribute_value.sql
+++ sql_add_code_couleur_dans_llx_product_attribute_value.sql
@@ -1,2 +1,3 @@
 ALTER TABLE `llx_product_attribute_value` ADD `code_couleur` VARCHAR(255) NULL AFTER `value`; 
 ALTER TABLE `llx_product_attribute_value` ADD `type_taille` VARCHAR(255) NULL AFTER `code_couleur`; 
+ALTER TABLE `llx_product_attribute_value` ADD `image_couleur` VARCHAR(255) NULL AFTER `type_taille`; 
--- sql_add_field_qty_commander_qty_fab_prix_yuan_composition.sql
+++ sql_add_field_qty_commander_qty_fab_prix_yuan_composition.sql
@@ -10,6 +10,7 @@ alter table llx_product add weight_variant double(24,2) DEFAULT NULL;
 alter table llx_product add ref_fab_frs VARCHAR(255) NULL;
 alter table llx_product add taux_euro_yuan double(24,3) DEFAULT NULL;
 
-alter TABLE llx_product MODIFY weight_variant double(24,3) DEFAULT NULL;
-
 alter table llx_product add ref_tissus_couleur VARCHAR(255) NULL;
+
+alter table llx_product add icone_prod_1 VARCHAR(255) NULL;
+alter table llx_product add icone_prod_2 VARCHAR(255) NULL;
